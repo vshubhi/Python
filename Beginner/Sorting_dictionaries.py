68c524b93aa5917b7d20ce7ef0dbdaca2b90b60c
Shubhi Verma <shubhi_verma@epam.com>
2023-08-28 19:16:47 +0530

    Python Beginner commit 5

--- Beginner/Sorting_dictionaries.py
+++ Beginner/Sorting_dictionaries.py
@@ -5,6 +5,6 @@ Dictionaries cannot be sorted directly,
 but you can create a sorted list of keys or 
 key-value pairs using the sorted() function:
 """
-
+my_dict = {"a":26, "b": 25, "z":1, "y":2, "x":3} 
 sorted_keys = sorted(my_dict.keys())
 sorted_items = sorted(my_dict.items(), key=lambda x: x[1])
