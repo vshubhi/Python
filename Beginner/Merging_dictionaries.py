9c891a1adc5cbd87535cef14adbeb73971a14938
Shubhi Verma <shubhi_verma@epam.com>
2023-08-28 19:10:51 +0530

    Python Beginner commit 3

---
+++ Beginner/Merging_dictionaries.py
@@ -0,0 +1,12 @@
+"""
+Merging dictionaries: 
+
+To merge dictionaries, you can use the update() method 
+or dictionary unpacking (Python 3.5+). 
+
+Here's how to use update():
+"""
+
+dict1 = {'a': 1, 'b': 2}
+dict2 = {'b': 3, 'c': 4}
+dict1.update(dict2)
