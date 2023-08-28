096c28b27d6e7e8b5d4dd3894ff6c7111a5f0c82
Shubhi Verma <shubhi_verma@epam.com>
2023-08-28 19:09:04 +0530

    Python Beginner commit 2

---
+++ Beginner/packing_unpacking_arguments.py
@@ -0,0 +1,9 @@
+"""
+Packing and unpacking of arguments: 
+
+Packing refers to combining multiple values into a single data structure 
+(like a tuple). Unpacking is the process of extracting values from a data structure 
+and assigning them to individual variables. For function arguments, 
+*args is used to pack arguments into a tuple, 
+while **kwargs is used to pack keyword arguments into a dictionary.
+"""
